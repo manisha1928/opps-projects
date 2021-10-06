6e4d0c87bbcc28abd1802b847f972f7d2b6ff83a
manisha1928 <91174169+manisha1928@users.noreply.github.com>
2021-10-06 10:46:50 +0530

    Create task1.cpp

---
+++ practical-6/task1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int n;
+ cout<<"Enter a integer:"<<endl;
+ cin>>n;
+ string s;
+ cout<<"Enter the string:" <<endl;
+ getline(cin,s) ;
+ 
+ cout<<(n+s);
+ return 0;
+
+}
+  
