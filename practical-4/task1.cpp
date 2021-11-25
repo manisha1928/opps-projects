78414acc9926f2a1fc8b6afc4c3fc9ae9bfbdde4
manisha1928 <91174169+manisha1928@users.noreply.github.com>
2021-11-25 22:08:58 +0530

    Update task1.cpp

--- practical-4/task1.cpp
+++ practical-4/task1.cpp
@@ -3,24 +3,23 @@
 using namespace std;
 namespace first
 {
-  void add(int a,int b)
-   {
-   cout<<a+b<<endl;
-   }
+    int sum(int a,int b)
+         {return a+b;}
 }
-
 namespace second
 {
-  void add(float a,float b)
-   {
-   cout<<a+b<<endl;
-   }
+     float sum(float a,float b)
+          {return a+b;}
 }
+using namespace first;
+using namespace second;
 int main()
 {
-  first:: add(1, 5);
-  second:: add(1.5, 1.5);
-
-return 0 ;
+     int integer=0;
+     integer=first::sum(3.1,4);
+    float floatnum=0;
+    floatnum=second::sum(2.0,5.4);
+    cout<<"Sum of integers: "<<integer<<"\n";
+    cout<<"Sum of float: "<<floatnum<<"\n";
+ return 0;
 }
-
