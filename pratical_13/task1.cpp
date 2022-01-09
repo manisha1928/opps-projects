f24b921a1bb0b5e0baec10f9eb6d36bc6b07c407
manisha1928 <91174169+manisha1928@users.noreply.github.com>
2022-01-09 20:52:02 +0530

    Update task1.cpp

--- pratical_13/task1.cpp
+++ pratical_13/task1.cpp
@@ -1,1 +1,32 @@
 
+/*Version 1: without Using "using keyword" 
+ 2. The function not visible to child class
+    are both add function of Base Class*/
+#include<iostream>
+using namespace std;
+class Base 
+{
+    public: 
+    int add(int a,int b)
+    {
+        return(a+b);
+    } 
+    float add(float a,float b)
+    {
+        return(a+b);
+    } 
+}; 
+class child:public Base{
+    public: 
+    int add(int a,int b)
+    {
+        return(a+b+1);
+    }
+}; 
+int main()
+{
+    child c;
+    cout<<c.add(2,3)<<endl;
+    cout<<c.add(2.1,3.1)<<endl; 
+    return 0;
+}
