363f503117bd6f0d110a765ef367ae1882da94fa
manisha1928 <91174169+manisha1928@users.noreply.github.com>
2022-01-09 12:11:54 +0530

    Create task3_v3.cpp

---
+++ practical_11/task3_v3.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class A
+{
+	public:
+		void display()
+		{
+			cout<<"This is function of A"<<endl;
+		}
+};
+class B
+{
+	public:
+		void display()
+		{
+			cout<<"This is function of B"<<endl;
+		}
+};
+class C: public A, public B{};
+int main()
+{
+	C obj;
+	obj.A::display();
+	obj.B::display();
+	return 0;
+}
